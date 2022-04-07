15aee66824dfb5364e89736e46da3ddecaf9a478
Noam Blum <noamblum756@gmail.com>
2022-04-07 21:01:24 +0300

    Added splash screen

---
+++ app/src/main/java/com/mechonot/fiddle/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.mechonot.fiddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Objects;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            //This method will be executed once the timer is over
+            // Start your app main activity
+            Intent i = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(i);
+            // close this activity
+            finish();
+        }, 2000);
+    }
+}
