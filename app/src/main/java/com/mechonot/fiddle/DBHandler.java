522fe2b9dea3643188f97ee2540b8026f29ee743
Roy Abend <roy@evolution.inc>
2022-04-07 11:45:34 +0300

    database

--- app/src/main/java/com/mechonot/fiddle/DBHandler.java
+++ app/src/main/java/com/mechonot/fiddle/DBHandler.java
@@ -5,32 +5,37 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.Date;
+
 public class DBHandler extends SQLiteOpenHelper {
 
     // creating a constant variables for our database.
     // below variable is for our database name.
-    private static final String DB_NAME = "coursedb";
+    private static final String DB_NAME = "fiddle";
 
     // below int is our database version
     private static final int DB_VERSION = 1;
 
-    // below variable is for our table name.
-    private static final String TABLE_NAME = "mycourses";
+    private static final String TABLE_NAME = "fids";
 
-    // below variable is for our id column.
     private static final String ID_COL = "id";
 
-    // below variable is for our course name column
-    private static final String NAME_COL = "name";
+    private static final String CREATION_DATE_COL = "creation_date";
 
-    // below variable id for our course duration column.
     private static final String DURATION_COL = "duration";
 
-    // below variable for our course description column.
-    private static final String DESCRIPTION_COL = "description";
+    private static final String BODY_COL = "body";
 
-    // below variable is for our course tracks column.
-    private static final String TRACKS_COL = "tracks";
+    private static final String IS_LOCAL_COL = "is_local";
+
+    private static final String DEAD_LINE_COL = "dead_line";
+
+    private static final String PRIORITY_COL = "priority";
+
+    private static final String INTERVAL_COL = "interval";
+
+    private static final String NUM_INTERVALS_COL = "num_intervals";
+
 
     // creating a constructor for our database handler.
     public DBHandler(Context context) {
@@ -46,10 +51,14 @@ public class DBHandler extends SQLiteOpenHelper {
         // along with their data types.
         String query = "CREATE TABLE " + TABLE_NAME + " ("
                 + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + NAME_COL + " TEXT,"
-                + DURATION_COL + " TEXT,"
-                + DESCRIPTION_COL + " TEXT,"
-                + TRACKS_COL + " TEXT)";
+                + CREATION_DATE_COL + " TEXT,"
+                + DURATION_COL + " INTEGER,"
+                + BODY_COL + " TEXT,"
+                + IS_LOCAL_COL + " INTEGER,"
+                + DEAD_LINE_COL + " TEXT,"
+                + PRIORITY_COL + " INTEGER,"
+                + INTERVAL_COL + " INTEGER,"
+                + NUM_INTERVALS_COL + " INTEGER)";
 
         // at last we are calling a exec sql
         // method to execute above sql query
@@ -57,7 +66,15 @@ public class DBHandler extends SQLiteOpenHelper {
     }
 
     // this method is use to add new course to our sqlite database.
-    public void addNewCourse(String courseName, String courseDuration, String courseDescription, String courseTracks) {
+    public void addNewCourse(Integer id,
+                             String create_date,
+                             Integer duration,
+                             String body,
+                             Integer is_local,
+                             String dead_line,
+                             Integer priority_col,
+                             Integer interval,
+                             Integer num_intervals) {
 
         // on below line we are creating a variable for
         // our sqlite database and calling writable method
@@ -70,10 +87,14 @@ public class DBHandler extends SQLiteOpenHelper {
 
         // on below line we are passing all values
         // along with its key and value pair.
-        values.put(NAME_COL, courseName);
-        values.put(DURATION_COL, courseDuration);
-        values.put(DESCRIPTION_COL, courseDescription);
-        values.put(TRACKS_COL, courseTracks);
+        values.put(ID_COL, id);
+        values.put(CREATION_DATE_COL, create_date);
+        values.put(DURATION_COL, duration);
+        values.put(BODY_COL, body);
+        values.put(IS_LOCAL_COL, is_local);
+        values.put(DEAD_LINE_COL, dead_line);
+        values.put(PRIORITY_COL, priority_col);
+        values.put(NUM_INTERVALS_COL, num_intervals);
 
         // after adding all values we are passing
         // content values to our table.
