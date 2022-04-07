56a529b0e93fa935a7d3e9d7ca7bc04eae62ec86
Noam Blum <noam@d-bur.com>
2022-04-07 12:40:56 +0300

    Added basic fid scrolling activity. Closes #4

---
+++ app/src/main/java/com/mechonot/fiddle/scrolling/FauxFid.java
@@ -0,0 +1,17 @@
+package com.mechonot.fiddle.scrolling;
+
+public class FauxFid {
+
+    // Getters and setters
+    public String getTitle() {
+        return "I am a Fid";
+    }
+
+    public String getDescription() {
+        return "A very interesting description";
+    }
+
+    public int getTime() {
+        return 5;
+    }
+}
