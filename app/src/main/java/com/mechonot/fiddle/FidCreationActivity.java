165eaf6de502631df3676144b0eba80b25bf5d3b
ereldebel <erel.debel@mail.huji.ac.il>
2022-04-07 16:39:03 +0300

    Started Fid Creation Activity from scratch

--- app/src/main/java/com/mechonot/fiddle/FidCreationActivity.java
+++ app/src/main/java/com/mechonot/fiddle/FidCreationActivity.java
@@ -1,33 +1,13 @@
 package com.mechonot.fiddle;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
+import android.os.Bundle;
 
 public class FidCreationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fid_creation_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.settings, new SettingsFragment())
-                    .commit();
-        }
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
+        setContentView(R.layout.activity_fid_creation);
     }
 }
