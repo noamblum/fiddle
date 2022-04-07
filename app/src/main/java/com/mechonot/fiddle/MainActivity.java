38046d613de3783e907dc4ec422d024c432f1b97
Noam Blum <noamblum756@gmail.com>
2022-04-07 15:28:58 +0300

    Fix previous commit overriden changes

--- app/src/main/java/com/mechonot/fiddle/MainActivity.java
+++ app/src/main/java/com/mechonot/fiddle/MainActivity.java
@@ -2,100 +2,42 @@ package com.mechonot.fiddle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
+import com.mechonot.fiddle.scrolling.FidScrollingActivity;
+import com.mechonot.fiddle.utils.OnSwipeTouchListener;
 
-    private  static final String TAG = "swipe position";
-    private float x1, x2, y1, y2;
-    private static int MIN_DISTANCE = 150;
-    private GestureDetector gestureDetector;
+public class MainActivity extends AppCompatActivity{
 
+    private OnSwipeTouchListener swipeTouchListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
         //Initialize GestureDetector
-        this.gestureDetector = new GestureDetector(MainActivity.this, this);
+        this.swipeTouchListener = new OnSwipeTouchListener(MainActivity.this) {
+            @Override
+            public void onSwipeRight() {
+                Intent intent = new Intent(getBaseContext(), FidScrollingActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+            }
+        };
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
-
-        gestureDetector.onTouchEvent(event);
-        switch (event.getAction()){
-            //starting to swipe time gesture
-            case MotionEvent.ACTION_DOWN:
-                x1 = event.getX();
-                //y1 = event.getY();
-                break;
-            //ending to swipe time gesture
-            case MotionEvent.ACTION_UP:
-                x2 = event.getX();
-               // y2 = event.getY();
-
-                //getting values for horizontal swipe
-                float valueX = x2 - x1;
-
-                //getting values for vertical swipe
-                //float valueY = y2 - y1;
-
-                if(Math.abs(valueX) > MIN_DISTANCE){
-                    //detect left to right
-                    if(x2 > x1){
-                        Toast.makeText(this, "Right is swiped", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, "right swipe");
-                    }
-                    //detect right to left
-                    else {
-                        Toast.makeText(this, "Left is swiped", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, "left swipe");
-                    }
-
-                }
-
-
-                break;
-
-        }
-
-
+        swipeTouchListener.onTouch(getCurrentFocus(), event);
         return super.onTouchEvent(event);
-    }
-
-    @Override
-    public boolean onDown(MotionEvent motionEvent) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent motionEvent) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent motionEvent) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent motionEvent) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        return false;
     }
 }
